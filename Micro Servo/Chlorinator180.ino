f4be2ac72c41b88ca0b090dfe9db8e194e930798
ZhiJie88 <111533492+ZhiJie88@users.noreply.github.com>
2022-11-15 13:18:21 -0600

    Updated comments to match syntax for C#

--- Micro Servo/Chlorinator180.ino
+++ Micro Servo/Chlorinator180.ino
@@ -1,32 +1,75 @@
+/* ChlorinatorTDS.ino
+ * Author: Christopher Xu - KSU ME 574 Group 16
+ */
+
+///<summary>
+/// Creates initial references
+///</summary>
 #include <Servo.h>
 #define TdsSensorPin A1
-#define VREF 3.3      																																								                                                                              // analog reference voltage(Volt) of the ADC
-#define SCOUNT  30           																																						                                                                            // sum of sample point
-
-int analogBuffer[SCOUNT];    																																						                                                                            // store the analog value in the array, read from ADC
-int analogBufferTemp[SCOUNT];	
-int analogBufferIndex = 0, copyIndex = 0;
-float averageVoltage = 0, tdsValue = 0, temperature = 25;
+#define VREF 3.3                                                                                                                                                                  
+#define SCOUNT  30                                                                                                                                                                
 Servo s;
+
+///<summary>
+///Initializes the analog buffer
+///</summary>
+int analogBuffer[SCOUNT];                                                                                                                                                          
+
+///<summary>
+///Initializes the temp analog buffer
+///</summary>
+int analogBufferTemp[SCOUNT]; 
+
+///<summary>
+/// Creates indexers for the arrays
+///</summary>
+int analogBufferIndex = 0, copyIndex = 0;
+
+///<summary>
+/// Initialize values to 0
+///</summary>
+float averageVoltage = 0, tdsValue = 0, temperature = 25;
+
+
+///<summary>
+/// Initializes the position value to zero
+///</summary>
 int currPos = 0;
 //Chlorine is around 1 - 3.00 ppm for pools and spas respectivly
-const float absTDSvalue = 850;
+
+///<summary>
+/// Upper limit for TDS values
+///</summary>
+const float absTDSvalue = 350;
+
+///<summary>
+/// Initializes previous poistion to the current position initailliy
+///</summary>
 int prevPos = currPos;
 
+
+///<summary>
+///Initializes all components, sets the TDS to pin A1 and the servo to pin 10
+///</summary>
 void setup()
 {
   Serial.begin(19200);
   pinMode(TdsSensorPin, INPUT);
   s.attach(10);
 }
-  
+
+
+///<summary>
+///Arduino loop, operates on a set of timers to read and compute data, moving the dispensing mechanism if data is within range
+///</summary>
 void loop()
 {
   static unsigned long analogSampleTimepoint = millis();
-  if (millis() - analogSampleTimepoint > 50U)  																																		                                                                //every 40 milliseconds,read the analog value from the ADC
+  if (millis() - analogSampleTimepoint > 50U)                                                                               
   {
     analogSampleTimepoint = millis();
-    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);    																													                                                      //read the analog value and store into the buffer
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);  
     analogBufferIndex++;
     if (analogBufferIndex == SCOUNT)
       analogBufferIndex = 0;
@@ -34,22 +77,26 @@ void loop()
   static unsigned long printTimepoint = millis();
   if (currPos == 0 || currPos == 180)
   {
-	  s.write(currPos);
+    s.write(currPos);
   }
   if (millis() % 10000U == 0 && millis()-printTimepoint > 5000U)
   {
     for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
       analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
-    averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 1024.0; 																								                                              // read the analog value more stable by the median filtering algorithm, and convert to voltage value
-    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0); 																												                                                    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
-    float compensationVolatge = averageVoltage / compensationCoefficient; 																											                                                  //temperature compensation
-    tdsValue = (133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 255.86 * compensationVolatge * compensationVolatge + 857.39 * compensationVolatge);		//convert voltage value to tds value
+    averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 1024.0;                                                                                              
+    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);                                                                                                            
+    float compensationVolatge = averageVoltage / compensationCoefficient;                                                                                                        
+    tdsValue = (133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 255.86 * compensationVolatge * compensationVolatge + 857.39 * compensationVolatge);   
     Serial.print("voltage:");
     Serial.print(averageVoltage,2);
     Serial.print("V   ");
     Serial.print("TDS----Value:");
     Serial.print(tdsValue, 4);
     Serial.println("ppm");
+    if (tdsValue == 0)
+    {
+      tdsValue = 0.00001;
+    }
   }
   float prevTDS = 0;
   if (tdsValue != 0 && prevTDS != tdsValue && tdsValue < absTDSvalue)
@@ -90,6 +137,13 @@ void loop()
   }
 }
 
+
+///<summary>
+///From a given list of data gathered from a sensor, an average number is computed
+///</summary>
+///<param name="bArray">Array of sensor readings</param>
+///<param name="iFilterLen">Length of the array of sensor readings</param>
+///<return>The average sensor reading in a given array of sensoring readings<\returns>
 int getMedianNum(int bArray[], int iFilterLen)
 {
   int bTab[iFilterLen];
